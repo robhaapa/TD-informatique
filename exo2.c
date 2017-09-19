d83aa8df71ab467c5b977b18315dc7dcdd49d036
Moumoute73 <32091744+Moumoute73@users.noreply.github.com>
2017-09-19 12:36:54 +0200

    Add files via upload

--- exo2.c
+++ exo2.c
@@ -1,43 +1,66 @@
-#include <uvsqgraphics.h>
+#include "uvsqgraphics.h"
 
 int main()
 {
-	init_graphics(1500, 1000);
-	POINT P1, P2, P3;
-	POINT L1, L2, L3, L4, L5;
-	POINT M1, M2, M3, M4, M5;
+	init_graphics(900,600);
 	
-	P1.x = 100; P1.y = 300;
-	P2.x = 150; P2.y = 300;
-	P3.x = 200; P3.y = 300;
+	POINT g;
+	POINT d;
 	
-	L1.x = 400; L1.y = 350;
-	L2.x = 400; L2.y = 450;
-	L3.x = 400; L3.y = 550;
-	L4.x = 400; L4.y = 650;
-	L5.x = 400; L5.y = 750;
+	g.x= 100; g.y= 300;
+	d.x= 800; d.y= 300;
+	draw_line(g,d,rouge);
 	
-	M1.x = 700; M1.y = 350;
-	M2.x = 700; M2.y = 450;
-	M3.x = 700; M3.y = 550;
-	M4.x = 700; M4.y = 650;
-	M5.x = 700; M5.y = 750;
+	draw_circle(g,100,bleu);
+	draw_circle(d,100,vert);
 	
-	draw_line(P1, P3, pink);
-	draw_circle(P1, 25, silver);
-	draw_circle(P2, 25, gold);
-	draw_circle(P3, 25, violet);
+	POINT m;
 	
-	draw_line(L1, M1, blue);
-	draw_line(L2, M2, orange);
-	draw_line(L3, M3, brown);
-	draw_line(L4, M4, yellow);
-	draw_line(L5, M5, pink);
+	m.x= 450; m.y= 300;
+	draw_circle(m,100,magenta);
 	
+	
+	//Partie 2
+	
+	g.y=400; d.y=400;
+	draw_line(g,d,vert);
+	
+	//Partie 3 (exo 2)
+	POINT bg;
+	POINT hd;
+	
+	bg.x=20; bg.y=20;
+	hd.x=220; hd.y=120;
+	
+	draw_rectangle(bg,hd,blanc);
+	
+	//Partie 4 
+	
+	POINT bd, hg;
+	bg.x= 200; bg.y= 200;
+	bd.x=300; bd.y=200;
+	hg.x=200; hg.y=300;
+	hd.x=300; hd.y=300;
+	
+	draw_line(bg, bd, bleu);
+	draw_line(bd, hd, rouge);
+	draw_line(hd, hg, vert);
+	draw_line(hg, bg, jaune);
+	
+	POINT a,b;
+	
+	a.x=100; a.y= 150;
+	b.x=150; b.y= 200;
+	
+	draw_line(a,b,rouge);
+	// Exercice 3
+	
+	
+	
+	
+
+    
 	
 	wait_escape();
-	return(0);
+exit(0);
 }
-	
-	
-	
